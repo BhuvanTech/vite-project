e632c720eef8cd9e29c5fb22522c27e004940003
BhuvanTech <bhuvanye@gmail.com>
2024-06-03 09:24:53 +0530

    Leaning React + Vite + Typescript
    Todo Application completed.

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from '../types/todo';
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: 'Eat 😋',
+    completed: false,
+  },
+
+  {
+    id: 2,
+    title: 'Code 😎',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'sleep 😴',
+    completed: false,
+  },
+];
